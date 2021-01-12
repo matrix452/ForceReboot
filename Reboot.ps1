c3475fd408421ec495950b6893cd6748db7e4b7f
matrix452 <61765417+matrix452@users.noreply.github.com>
2021-01-12 13:11:48 -0500

    Create Reboot.ps1

---
+++ Reboot.ps1
@@ -0,0 +1,27 @@
+# Initalize the Variables
+$Hour1= 0
+$Min1= 0
+$second1 = 0
+$totalseconds = 0
+$SecondsLeft = 0
+#Get current time
+$Hour1 = (get-Date).Hour;
+$Min1 = (get-date).Minute;
+$second1 = (get-date).Second;
+#Convert Current time to Seconds
+$totalseconds = $second1 + ($Min1 * 60) + ($Hour1 * 3600);
+#Set Reboot time, hour in 24 time * 3600, to add something other than on the hour, add (MM * 60) 
+$RebootTime = (22 *3600)
+#Calculate Time left
+$SecondsLeft = $RebootTime - $totalseconds
+#Loop to countdown
+while ($SecondsLeft -gt 0 )
+{
+    $Hour1 = (get-Date).Hour;
+    $Min1 = (get-date).Minute;
+    $second1 = (get-date).Second;
+    $totalseconds = $second1 + ($Min1 * 60) + ($Hour1 * 3600);
+    $SecondsLeft = $RebootTime - $totalseconds
+}
+#Force Reboot
+Restart-Computer -Force
